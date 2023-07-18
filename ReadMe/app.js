358c6b6665eefeb776c5ba53facec30d0ce674da
guidosalustri <guido.sabbsal@gmail.com>
2023-07-18 16:08:26 -0300

    home frame inicial

---
+++ ReadMe/app.js
@@ -0,0 +1,12 @@
+const express= require("express")
+const path= require("path")
+const app=express();
+
+app.use(express.static("public"));
+
+app.listen(3000,()=> console.log("srever on"));
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+
+});
