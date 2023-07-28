01ed4732a161fbae107b1a891ba9c3073945c3ce
Lucasfullstack <cai_luk@yahoo.com>
2023-07-27 21:06:02 -0300

    color footer

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ app.use(express.static("public"));
 
 app.listen(3000,()=> console.log("srever on"));
 
+
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"));
+
+});
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+
+});
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"));
 
 });
