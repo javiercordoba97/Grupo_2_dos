420d6e4280862b80497b7f16adb5c631d89c6b9d
Lucasfullstack <cai_luk@yahoo.com>
2023-08-08 21:33:00 -0300

    clase en vivo cambios

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/home.html"));
 
 });
+app.get("/Logo",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+
+});
 app.get("/register",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/register.html"));
 
