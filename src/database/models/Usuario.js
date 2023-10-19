02e661b513dd1df7b4288d97739b1d7fd3d82ff5
javiercordoba97 <cordobavjavier@gmail.com>
2023-10-19 03:07:18 -0300

    agregando usuario a models

---
+++ src/database/models/Usuario.js
@@ -0,0 +1,48 @@
+const { Sequelize } = require(".")
+
+module.exports = (sequelize, dataTypes) => {
+    let alias = "usuarios"
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre:{
+            type: dataTypes.STRING
+        },
+        apellido:{
+            type: dataTypes.STRING
+        },
+        email:{
+            type: dataTypes.STRING(40)
+        },
+        telefono:{
+            type: dataTypes.INTEGER
+        },
+        foto:{
+            type: dataTypes.STRING(255)
+        },
+        created:{
+            type: dataTypes.DATEONLY
+        },
+        updated:{
+            type: dataTypes.DATEONLY
+        },
+        deleted:{
+            type: dataTypes.DATEONLY
+        }
+    }
+    let config = {
+        tableName: "usuarios",
+        timestamps: true,
+        paranoid: true,
+        deletedAt: "deleted",
+        createdAt: "created",
+        updatedAt: "updated"
+    }
+    const usuarios = sequelize.define(alias, cols, config)
+
+
+    return usuarios
+} 
