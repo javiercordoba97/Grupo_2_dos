49ac70c8cee5fd2a9da95328472a3d894da3ae0b
guidosalustri <guido.sabbsal@gmail.com>
2023-11-12 16:46:31 -0300

    validacion front del register

---
+++ public/js/registerValidation.js
@@ -0,0 +1,64 @@
+window.onload = function(){
+
+
+    const imputName= document.querySelector("#name")
+    imputName.focus()
+    const form = document.querySelector(".form")
+    form.addEventListener("submit",(e)=>{
+        e.preventDefault()
+        if(form.name.value==""){
+            form.name.classList.add("invalid")
+        } else{
+            form.name.classList.remove("invalid")
+        }
+        if(form.last_name.value==""){
+            form.last_name.classList.add("invalid")
+        } else{
+            form.last_name.classList.remove("invalid")
+        }
+        if(form.password.value==""){
+            form.password.classList.add("invalid")
+        } else{
+            form.password.classList.remove("invalid")
+        }
+        if(form.confirmPassword.value==""){
+            form.confirmPassword.classList.add("invalid")
+        } else{
+            form.confirmPassword.classList.remove("invalid")
+        }
+        console.log(form.confirmPassword.value)
+        console.log(form.password.value)
+        if(form.confirmPassword.value!=form.password.value){
+            form.confirmPassword.classList.add("invalid")
+            const err = document.querySelector(".err")
+            err.innerHTML = "<li>Password must match</li>"
+            err.classList.add("error")
+        } else{
+            form.confirmPassword.classList.remove("invalid")
+            const err = document.querySelector(".err")
+            err.innerHTML =""
+            err.classList.remove("error")
+        }
+        if(form.dni.value==""){
+            form.dni.classList.add("invalid")
+        } else{
+            form.dni.classList.remove("invalid")
+        }
+        if(form.tel.value==""){
+            form.tel.classList.add("invalid")
+        } else{
+            form.tel.classList.remove("invalid")
+        }
+        if(form.address.value==""){
+            form.address.classList.add("invalid")
+        } else{
+            form.address.classList.remove("invalid")
+        }
+        if(form.email.value==""){
+            form.email.classList.add("invalid")
+        } else{
+            form.email.classList.remove("invalid")
+        }
+
+    })
+}
