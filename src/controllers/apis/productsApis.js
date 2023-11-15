c0f323c1e290c7ca733a272986e6bb93523b2608
Lucasfullstack <cai_luk@yahoo.com>
2023-11-14 21:53:21 -0300

    REACT cambiando nombre de los modelos

--- src/controllers/apis/productsApis.js
+++ src/controllers/apis/productsApis.js
@@ -5,27 +5,27 @@ const controller ={
     list: async (req,res)=>{
         let respuesta = {
             count: 0,
-            countCategory:{},
+            countByCategory:{},
             products: []
         }
-        const [products,categories] = await Promise.all([db.Product.findAll({include:[{association: "Category"}]}),db.Category.findAll({include:[{association: "Product"}]})])
+        const [products,categories] = await Promise.all([db.Juego.findAll({include:[{association: "generos"}]}),db.generos.findAll({include:[{association: "Juegos"}]})])
         respuesta.count = products.length
         categories.forEach(categoria =>{
-            respuesta.countByCategory[categoria.name] = categoria.Product.length
+            respuesta.countByCategory[categoria.nombre] = categoria.Juegos.length
         });
             respuesta.products = products.map(row=>{
                 return{
                     id: row.id,
                     name: row.name,
                     description: row.description,
-                    category: row.Category,
+                    category: row.generos,
                     detail: "/api/product/detail/"+row.id
                 }
             });
             res.json(respuesta)
     },
     detail: async (req,res)=>{
-        let product = await db.Product.findByPk(req.params.id);
+        let product = await db.Juego.findByPk(req.params.id);
         let respuesta = {
             ...product,
             url_imagen: "img/products/"+product.img,
