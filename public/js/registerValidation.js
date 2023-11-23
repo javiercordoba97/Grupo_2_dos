8eead176c4b9b5c27992bb7ef5f0c198ce79c6e0
guidosalustri <guido.sabbsal@gmail.com>
2023-11-23 15:44:51 -0300

    solucion de form.submit registro de usuarios

--- public/js/registerValidation.js
+++ public/js/registerValidation.js
@@ -6,18 +6,23 @@ window.onload = function(){
     const form = document.querySelector(".form")
     form.addEventListener("submit",(e)=>{
         e.preventDefault()
+        var completarCampo=""
+        const err = document.querySelector(".err")
         if(form.name.value==""){
             form.name.classList.add("invalid")
+            completarCampo="Falta completar campos"
         } else{
             form.name.classList.remove("invalid")
         }
         if(form.last_name.value==""){
             form.last_name.classList.add("invalid")
+            completarCampo="Falta completar campos"
         } else{
             form.last_name.classList.remove("invalid")
         }
         if(form.password.value==""){
             form.password.classList.add("invalid")
+            completarCampo="Falta completar campos"
         } else{
             form.password.classList.remove("invalid")
         }
@@ -25,30 +30,36 @@ window.onload = function(){
         console.log(form.password.value)*/
         if(form.confirmPassword.value!=form.password.value){
             form.confirmPassword.classList.add("invalid")
-            const err = document.querySelector(".err")
+            /*const err = document.querySelector(".err")*/
             err.innerHTML = "<li>Password must match</li>"
             err.classList.add("alert_warning")
         } else{
             form.confirmPassword.classList.remove("invalid")
-            const err = document.querySelector(".err")
+            /*const err = document.querySelector(".err")*/
             err.innerHTML =""
             err.classList.remove("alert_warning")
         }
         if(form.confirmPassword.value=="" || form.confirmPassword.value!=form.password.value){
             form.confirmPassword.classList.add("invalid")
+            if(form.confirmPassword.value==""){completarCampo="Falta completar campos"}
         } else{
             form.confirmPassword.classList.remove("invalid")
         }
         if(form.tel.value==""){
             form.tel.classList.add("invalid")
+            completarCampo="Falta completar campos"
         } else{
             form.tel.classList.remove("invalid")
         }
         if(form.email.value==""){
             form.email.classList.add("invalid")
+            completarCampo="Falta completar campos"
         } else{
             form.email.classList.remove("invalid")
         }
-        form.submit()
+        if (completarCampo==""){form.submit()}
+        else{
+            err.innerHTML += "<li>"+completarCampo+"</li>"
+            err.classList.add("alert_warning")}
     })
 }
